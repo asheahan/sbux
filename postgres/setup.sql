dcaed0300279e3ecba4c461f765984191860eaff
Andrew Sheahan <asheahan@gmail.com>
2017-08-08 21:29:16 -0700

    Add postgres setup

---
+++ postgres/setup.sql
@@ -0,0 +1,19 @@
+-- Database: sbux
+
+-- DROP DATABASE sbux;
+
+CREATE DATABASE sbux
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE TABLE tweets (
+	message text,
+    created_at timestamp,
+    geo text,
+    src text
+)
